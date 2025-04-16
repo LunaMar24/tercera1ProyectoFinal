5b91366bdbac3d916b0e6fb1fb9bab6c4c2bcded
Marcela Jimenez <marcelamja@gmail.com>
2025-04-15 22:29:05 -0600

    Ajustes en responsive Se agrega footer Se inicia hamburger

--- parte_inferior.php
+++ parte_inferior.php
@@ -1,7 +1,22 @@
-  <div class="pie-Datos">
+
+  <div>
     <div class="cards-list"></div>
   </div>
-</html>
+
+    <footer class="pie">
+      <div class="pie-Datos">
+        <div class="columna izquierda titulo">
+          <p>LunaMar Nails</p>
+        </div>
+        <div class="columna centro">
+          <p><b>Estilista:</b> Marcela Jiménez Arguedas</p>
+        </div>
+        <div class="columna derecha">
+          <p><b>Ver</b> 1.0.0</p>
+        </div>
+      </div>
+    </footer> 
+  </html>
 
 <script src="./scripts/main.js"></script>
 <script src="./scripts/accordion.js"></script>
--- parte_superior.php
+++ parte_superior.php
@@ -26,23 +26,32 @@
     </div>
 
     <div class="menu-down">
-        <div class="menu-izquierda">
-            <a href="#" onclick="cargarContenido('QuienesSomos.html')">Quienes Somos</a>
-            <a href="#" onclick="cargarContenido('Estilos.html')">Estilos de Diseños</a>
-            <a href="#" onclick="cargarContenido('Citas.html')">Citas</a>
-            <a href="#" onclick="cargarContenido('Contacto.html')">Contacto</a>
-        </div>
+        <div class="menu-container">
+            <div class="hamburger-icon">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
 
-        <div class="menu-derecha">
-            <a href="mailto:Marcemjav@hotmail.com">
-                <img src="./img/correo.png" alt="correo" height="35px">
-            </a>
-            <a href="https://www.facebook.com/share/1BkULDXwds/">
-                <img src="./img/facebook.png" alt="facebook" height="35px">
-            </a>
-            <a href="https://www.instagram.com/marcelajimenezarguedas?utm_source=qr&igsh=MTR5Zjc2eW45cTVxYQ==">
-                <img src="./img/instagram.png" alt="instagram" height="35px">
-            </a>
+            <div class="menu-izquierda">
+                <a href="#" onclick="cargarContenido('QuienesSomos.html')">Quienes Somos</a>
+                <a href="#" onclick="cargarContenido('Estilos.html')">Estilos de Diseños</a>
+                <a href="#" onclick="cargarContenido('Citas.html')">Citas</a>
+                <a href="#" onclick="cargarContenido('Contacto.html')">Contacto</a>
+            </div>
+
+            <div class="menu-derecha">
+                <a href="mailto:Marcemjav@hotmail.com">
+                    <img src="./img/correo.png" alt="correo" height="35px">
+                </a>
+                <a href="https://www.facebook.com/share/1BkULDXwds/">
+                    <img src="./img/facebook.png" alt="facebook" height="35px">
+                </a>
+                <a href="https://www.instagram.com/marcelajimenezarguedas?utm_source=qr&igsh=MTR5Zjc2eW45cTVxYQ==">
+                    <img src="./img/instagram.png" alt="instagram" height="35px">
+                </a>
+            </div>
         </div>
     </div>
 </menu>

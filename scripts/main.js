9717869e6ea394bb3134be6f740d7c1d052a07fc
Marcela Jimenez <marcelamja@gmail.com>
2025-04-25 17:06:12 -0600

    ajustes de comentarios

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,17 @@
+/*
+  Este método toma el archivo que se pasa por paráemtro, lee su contenido
+  y lo inserta dentro del div con id = "contenedor"
+  También luego de un momento, manda a inicializar un script si se definió un valor en el atributo data-obj
+*/
 function cargarContenido(pagina) {
     fetch(pagina)
     .then(response => response.text())
     .then(data => {
-        document.getElementById("contenedor").innerHTML = data;
+        document.getElementById("contenedor").innerHTML = data; //Inserta el HTML en el contenedor
 
         setTimeout(() => {
             let contenido = contenedor.querySelector("[data-obj]");
-            if (contenido) {
+            if (contenido) {                                           //Si se definió un data-obj se procede a llamar la función init de ese objeto
                 let funcion = contenido.getAttribute("data-obj");
                 if (funcion && typeof window[funcion] === "object") {
                     window[funcion].init();  // Ejecuta la función indicada en data-init                    
